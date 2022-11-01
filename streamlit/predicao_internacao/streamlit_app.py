f3aaeb95b76bc7a2fc0413194a91543ffc4f2434
Wallace Holanda <wallaceholanda@Mac-mini-de-Wallace.local>
2022-10-31 22:31:23 -0300

    feat: altera base de dados

--- streamlit/predicao_internacao/streamlit_app.py
+++ streamlit/predicao_internacao/streamlit_app.py
@@ -24,7 +24,7 @@ def criarModelo(modelo, baseObitoCurado, alvo):
 
 
 def importarModelo(modeloEscolhido):
-    url = 'streamlit/predicao_mortalidade/bases/ba-oc-6040-sp.xlsx'
+    url = 'streamlit/predicao_mortalidade/bases/ba-ic-7030-sp.xlsx'
     alvo = 'evolucaoCaso'
     baseObitoCurado = pd.read_excel(url, engine="openpyxl")
 
