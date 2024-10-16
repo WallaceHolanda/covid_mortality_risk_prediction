3bba16ec2b0fbb40a2e5807811bd130f24ce9cac
wallaceholanda <wallace.holanda@hotmail.com>
2024-10-16 13:07:20 -0300

    fix: change random forest parameter

--- streamlit/predicao_internacao/modelo.py
+++ streamlit/predicao_internacao/modelo.py
@@ -39,7 +39,7 @@ def criaAdaBoost():
 
 def criaRandomForest():
     return RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
-                                criterion='gini', max_depth=None, max_features='auto',
+                                criterion='gini', max_depth=None, max_features='sqrt',
                                 max_leaf_nodes=None, max_samples=None, min_impurity_decrease=0.0,
                                 min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, 
                                 n_estimators=100, n_jobs=-1, oob_score=False, random_state=7374, 
--- streamlit/predicao_mortalidade/modelo.py
+++ streamlit/predicao_mortalidade/modelo.py
@@ -40,7 +40,7 @@ def criaAdaBoost():
 # Retorna o Modelo Random Forest
 def criaRandomForest():
     return RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
-                           criterion='gini', max_depth=None, max_features='auto',
+                           criterion='gini', max_depth=None, max_features='sqrt',
                            max_leaf_nodes=None, max_samples=None,
                            min_impurity_decrease=0.0,
                            min_samples_leaf=1, min_samples_split=2,
